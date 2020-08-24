35e61ce505df63ea8829aa22b5999f1eba47c90d
Jarom Plewe <jplewe23@gmail.com>
2020-08-24 11:17:55 -0600

    added pseudocode

---
+++ assets/script/script.js
@@ -0,0 +1,21 @@
+
+// search
+    // use localStorage to keep histroy
+        // when user clicks search, value is stored to localStorage and displayed in history, stores val to a variable cityVal
+        // if they click on a city in the hitory, set it to form val()
+
+// ajax
+    // enter cityVal (from above) into the API url as well as the apiKey var
+    // .then run the function to append everything to the page -------- might have to use moment.js
+        // main card
+            // city and date in an h2 el
+            // temperature in a p el
+            // Humidity in a p el
+            // Wind Speed in a p el
+            // UV Index in a p el
+        // 5-day forecast
+            // 5 cards with the folowing info:
+                // date header
+                // emoji for weather
+                // Temp
+                // Humidity
