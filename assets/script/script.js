a73ef70eb7c3605b85bca4598e5dee6a91d6b4f2
Jarom Plewe <jplewe23@gmail.com>
2020-08-24 17:52:43 -0600

    added moment.js link in html

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,8 +14,6 @@ $('#submitBtn').on('click', function (event) {
     // create url with city form value
     let cityName = cityNameForm.val();
     let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-    // "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-    console.log(queryURL)
 
     // AJAX
     $.ajax({
@@ -23,7 +21,15 @@ $('#submitBtn').on('click', function (event) {
         method: "GET"
         // .then run the function to append everything to the page -------- might have to use moment.js
     }).then(function(response) {
-        console.log(response)
+
+        // main card
+            // city and date in an h2 el
+            
+            // temperature in a p el
+            // Humidity in a p el
+            // Wind Speed in a p el
+            // UV Index in a p el
+
         // .catch function for any errors that may occur
     }).catch(function(error) {
         console.log(error);
@@ -35,12 +41,7 @@ $('#submitBtn').on('click', function (event) {
 
 
 
-        // main card
-            // city and date in an h2 el
-            // temperature in a p el
-            // Humidity in a p el
-            // Wind Speed in a p el
-            // UV Index in a p el
+        
         // 5-day forecast
             // 5 cards with the folowing info:
                 // date header
