c42bc95120ad4b05447e865311911a79023b29ed
Jarom Plewe <jplewe23@gmail.com>
2020-08-24 22:23:28 -0600

    included moment.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,13 @@
 const apiKey = "97d917361d591ae19840f10060b31839";
 let cityNameForm = $('.cityName');
 let stateNameForm = $('.stateChoice');
+
 // search
 // use prepend to keep histroy
 // when user clicks search, value is prepended and displayed in history
 // if they click on a city in the hitory, set it to form val()
+
+console.log(moment().hour())
 
 $('#submitBtn').on('click', function (event) {
     event.preventDefault();
@@ -21,15 +24,24 @@ $('#submitBtn').on('click', function (event) {
         method: "GET"
         // .then run the function to append everything to the page -------- might have to use moment.js
     }).then(function(response) {
-
+        console.log(response)
         // main card
             // city and date in an h2 el
-            
+
             // temperature in a p el
             // Humidity in a p el
             // Wind Speed in a p el
             // UV Index in a p el
 
+
+        // 5-day forecast
+            // 5 cards with the folowing info:
+                // date header
+                // emoji for weather
+                // Temp
+                // Humidity
+                
+                
         // .catch function for any errors that may occur
     }).catch(function(error) {
         console.log(error);
@@ -42,11 +54,6 @@ $('#submitBtn').on('click', function (event) {
 
 
         
-        // 5-day forecast
-            // 5 cards with the folowing info:
-                // date header
-                // emoji for weather
-                // Temp
-                // Humidity
+        
 
 // !!!!!don't forget to ignoreDefault for the submit button!!!!
