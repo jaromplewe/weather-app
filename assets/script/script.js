9da708b190629905ee90e8403cfc6d8bc7858286
Jarom Plewe <jplewe23@gmail.com>
2020-08-25 14:48:08 -0600

    5-day forecast fills when go button is pressed.

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,11 @@ const apiKey = "97d917361d591ae19840f10060b31839";
 let cityName = $('.cityName');
 let stateNameForm = $('.stateChoice');
 const currentDate = moment().format("MMM" + " D, " + "YYYY");
+// 5-day forecast variables
+let forecastHeader = document.getElementsByClassName('5DayHeader');
+let forecastIcon = document.getElementsByClassName('5DayIcon');
+let forecastTemp = document.getElementsByClassName('5DayTemp');
+let forecastHumid = document.getElementsByClassName('5DayHumid');
 
 // search
 // use prepend to keep histroy
@@ -30,13 +35,13 @@ $('#submitBtn').on('click', function (event) {
         $('#cityDateDis').text(response.name + " - " + currentDate);
         
         // temperature
-        $('#mainTemp').text("Temperature: " + response.main.temp);
+        $('#mainTemp').text("Temperature: " + response.main.temp + " F");
 
         // Humidity
-        $('#mainHumid').text("Humidity: " + response.main.humidity);
+        $('#mainHumid').text("Humidity: " + response.main.humidity + "%");
         
         // Wind Speed
-        $('#mainWind').text("wind: " + response.wind.speed);
+        $('#mainWind').text("Wind: " + response.wind.speed);
 
         // UV Index
         // set lat and lon and create UV index URL
@@ -53,6 +58,7 @@ $('#submitBtn').on('click', function (event) {
             console.log(error);
         });
 
+        
 
         // 5-DAY FORECAST
         // create url for 5-day forecast
@@ -61,16 +67,75 @@ $('#submitBtn').on('click', function (event) {
             url: forecastUrl,
             method: "GET"
         }).then(function(response) {
-            console.log(response)
-            // date header
-            let next5DaysArr = [2, 10, 18, 26, 34];
-            for (let i=0; i<next5DaysArr.length; i++) {
-                let dateString = (response.list[i].dt_txt).slice(0, 10);
-                console.log(dateString);
-            }
-            // emoji for weather
-            // Temp
-            // Humidity
+
+            // create headers
+            // day 1
+            let forecastDate = (response.list[5].dt_txt).slice(0, 10);
+            forecastHeader[0].textContent = forecastDate;
+            // day 2
+            let forecastDate2 = (response.list[13].dt_txt).slice(0, 10);
+            forecastHeader[1].textContent = forecastDate2;
+            // day 3
+            let forecastDate3 = (response.list[21].dt_txt).slice(0, 10);
+            forecastHeader[2].textContent = forecastDate3;
+            // day 4
+            let forecastDate4 = (response.list[29].dt_txt).slice(0, 10);
+            forecastHeader[3].textContent = forecastDate4;
+            // day 5
+            let forecastDate5 = (response.list[37].dt_txt).slice(0, 10);
+            forecastHeader[4].textContent = forecastDate5;
+            
+            // create icons
+            // day 1
+            let iconUrl = "http://openweathermap.org/img/wn/" + response.list[5].weather[0].icon + "@2x.png"
+            forecastIcon[0].setAttribute('src', iconUrl);
+            // day 2
+            let iconUrl2 = "http://openweathermap.org/img/wn/" + response.list[13].weather[0].icon + "@2x.png"
+            forecastIcon[1].setAttribute('src', iconUrl2);
+            // day 3
+            let iconUrl3 = "http://openweathermap.org/img/wn/" + response.list[21].weather[0].icon + "@2x.png"
+            forecastIcon[2].setAttribute('src', iconUrl3);
+            // day 4
+            let iconUrl4 = "http://openweathermap.org/img/wn/" + response.list[29].weather[0].icon + "@2x.png"
+            forecastIcon[3].setAttribute('src', iconUrl4);
+            // day 5
+            let iconUrl5 = "http://openweathermap.org/img/wn/" + response.list[37].weather[0].icon + "@2x.png"
+            forecastIcon[4].setAttribute('src', iconUrl5);
+            
+            // create temp
+            // day 1
+            let forecastTempResponse = (response.list[5].main.temp);
+            forecastTemp[0].textContent = "Temp: " + forecastTempResponse + " F";
+            // day 2
+            let forecastTempResponse2 = (response.list[13].main.temp);
+            forecastTemp[1].textContent = "Temp: " + forecastTempResponse2 + " F";
+            // day 3
+            let forecastTempResponse3 = (response.list[21].main.temp);
+            forecastTemp[2].textContent = "Temp: " + forecastTempResponse3 + " F";
+            // day 4
+            let forecastTempResponse4 = (response.list[29].main.temp);
+            forecastTemp[3].textContent = "Temp: " + forecastTempResponse4 + " F";
+            // day 5
+            let forecastTempResponse5 = (response.list[37].main.temp);
+            forecastTemp[4].textContent = "Temp: " + forecastTempResponse5 + " F";
+
+            // create humidity
+            // day 1
+            let forecastHumidityResponse = (response.list[5].main.humidity);
+            forecastHumid[0].textContent = "Humidity: " + forecastHumidityResponse + "%";
+            // day 2
+            let forecastHumidityResponse2 = (response.list[13].main.humidity);
+            forecastHumid[1].textContent = "Humidity: " + forecastHumidityResponse2 + "%";
+            // day 3
+            let forecastHumidityResponse3 = (response.list[21].main.humidity);
+            forecastHumid[2].textContent = "Humidity: " + forecastHumidityResponse3 + "%";
+            // day 4
+            let forecastHumidityResponse4 = (response.list[29].main.humidity);
+            forecastHumid[3].textContent = "Humidity: " + forecastHumidityResponse4 + "%";
+            // day 5
+            let forecastHumidityResponse5 = (response.list[37].main.humidity);
+            forecastHumid[4].textContent = "Humidity: " + forecastHumidityResponse5+ "%";
+
         }).catch(function(error) {
             console.log(error);
         });
